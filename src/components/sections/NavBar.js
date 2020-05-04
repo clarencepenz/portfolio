3b92701446694cbdbb8deceb414ab3613d09488c
clarencepenz <clarencepenz@users.noreply.github.com>
2020-05-04 13:26:37 +0100

    brand name implemented

--- src/components/sections/NavBar.js
+++ src/components/sections/NavBar.js
@@ -3,9 +3,19 @@ import React, { Component } from 'react'
 export default class NavBar extends Component {
     render() {
         return (
-            <div style={{color: '#fff'}}>
-                NavBar
+            <div style={navBar}>
+                <h1 style={brand_name}>Clarence</h1>
             </div>
         )
     }
+}
+
+
+const navBar ={
+    color: '#fff'
+}
+
+const brand_name ={
+    paddingLeft: '90px',
+    paddingTop: '20px'
 }
