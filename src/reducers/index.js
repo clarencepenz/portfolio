1eabc879c6c669b492806d01c31b4136374af081
clarencepenz <clarencepenz@users.noreply.github.com>
2020-05-24 16:57:40 +0100

    update implemented

--- src/reducers/index.js
+++
@@ -1,9 +0,0 @@
-import { combineReducers } from 'redux'
-import { msgReducer } from './Message'
-import {projectReducer } from './Project'
-
-
-export const rootReducer = combineReducers({
-    msg: msgReducer,
-    project: projectReducer
-})
